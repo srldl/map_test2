30f2b1b148059cf01f7c2e8b5388dc9c5ca30f56
srldl <siri.uldal@gmail.com>
2015-10-06 11:08:33 +0200

    angular-leaflet-directive-0.8.5 working

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+ var app = angular.module("demoapp", ['leaflet-directive']);
+
+
+        app.controller("BasicFirstController", [ "$scope", function($scope) {
+
+
+
+            // Nothing here!
+  }]);
