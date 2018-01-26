43d1526ca18d5db7be8022de45b946bb5416eeb4
srldl <siri.uldal@gmail.com>
2018-01-26 13:50:38 +0100

    cluster test working

--- bundle.js
+++ bundle.js
@@ -28915,117 +28915,59 @@ var L = require('leaflet');
 require('leaflet.markercluster');
 L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
 
-
 var map_test2 = angular.module('map_test2',[]);
 
 
 map_test2.controller('MapCtrl', function($scope) {
+    console.log("ooo");
+
+   var addressPoints = [
+		[-37.8210922667, 175.2209316333, "2"],
+		[-37.8210819833, 175.2213903167, "3"],
+		[-37.8210881833, 175.2215004833, "3A"],
+		[-37.8211946833, 175.2213655333, "1"],
+		[-37.8209458667, 175.2214051333, "5"],
+		[-37.8208292333, 175.2214374833, "7"],
+		[-37.8325816, 175.2238798667, "537"],
+		[-37.8315855167, 175.2279767, "454"],
+		[-37.8096336833, 175.2223743833, "176"],
+		[-37.80970685, 175.2221815833, "178"],
+		[-37.8102146667, 175.2211562833, "190"],
+		[-37.8088037167, 175.2242227, "156"],
+		[-37.8112330167, 175.2193425667, "210"],
+		[-37.8116368667, 175.2193005167, "212"],
+		[-37.80812645, 175.2255449333, "146"],
+		[-37.8080231333, 175.2286383167, "125"],
+		[-37.8089538667, 175.2222222333, "174"],
+		[-37.8080905833, 175.2275400667, "129"],
+		[-37.808811, 175.2227592833, "172"],
+		[-37.80832975, 175.2276898167, "131"],
+		[-37.8089395333, 175.2281710333, "133"],
+		[-37.8093421, 175.2274883167, "135"],
+		[-37.8084820833, 175.22601925, "137"],
+		[-37.80881015, 175.22622865, "139"]
+	];
 
 	var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				maxZoom: 18,
-				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
 			}),
-			latlng = L.latLng(50.5, 30.51);
+			latlng = L.latLng(-37.82, 175.24);
 
-	var map = L.map('map', {center: latlng, zoom: 15, layers: [tiles]});
+		var map = L.map('map', {center: latlng, zoom: 13, layers: [tiles]});
+		console.log("vvv");
 
-    //Custom radius and icon create function
-		var markers = L.markerClusterGroup({
-			maxClusterRadius: 120,
-			iconCreateFunction: function (cluster) {
-				var markers = cluster.getAllChildMarkers();
-				var n = 0;
-				for (var i = 0; i < markers.length; i++) {
-					n += markers[i].number;
-				}
-				return L.divIcon({ html: n, className: 'mycluster', iconSize: L.point(40, 40) });
-			},
-			//Disable all of the defaults:
-			spiderfyOnMaxZoom: false, showCoverageOnHover: false, zoomToBoundsOnClick: false
-		});
+		var markers = L.markerClusterGroup();
 
-
-		function populate() {
-			for (var i = 0; i < 100; i++) {
-				var m = L.marker(getRandomLatLng(map), { title: i });
-				m.number = i;
-				markers.addLayer(m);
-			}
-			return false;
-		}
-		function populateRandomVector() {
-			for (var i = 0, latlngs = [], len = 20; i < len; i++) {
-				latlngs.push(getRandomLatLng(map));
-			}
-			var path = L.polyline(latlngs);
-			map.addLayer(path);
-		}
-		function getRandomLatLng(map) {
-			var bounds = map.getBounds(),
-				southWest = bounds.getSouthWest(),
-				northEast = bounds.getNorthEast(),
-				lngSpan = northEast.lng - southWest.lng,
-				latSpan = northEast.lat - southWest.lat;
-
-			return L.latLng(
-					southWest.lat + latSpan * Math.random(),
-					southWest.lng + lngSpan * Math.random());
+		for (var i = 0; i < addressPoints.length; i++) {
+			var a = addressPoints[i];
+			var title = a[2];
+			var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
+			marker.bindPopup(title);
+			markers.addLayer(marker);
 		}
 
-		populate();
 		map.addLayer(markers);
-
-
-
-		var shownLayer, polygon;
-
-		function removePolygon() {
-			if (shownLayer) {
-				shownLayer.setOpacity(1);
-				shownLayer = null;
-			}
-			if (polygon) {
-				map.removeLayer(polygon);
-				polygon = null;
-			}
-		};
-
-		markers.on('clustermouseover', function (a) {
-			removePolygon();
-
-			a.layer.setOpacity(0.2);
-			shownLayer = a.layer;
-			polygon = L.polygon(a.layer.getConvexHull());
-			map.addLayer(polygon);
-		});
-		markers.on('clustermouseout', removePolygon);
-		map.on('zoomend', removePolygon);
-
-
-  // var map = L.map('map2').setView([51.5, -0.09], 13);
-
-   //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    //  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //}).addTo(map);
-
-   //let arr = [[51.5, -0.10],[51.5, -0.09],[51.5, -0.11],[51.5, -0.12],[51.5, -0.13],[51.5, -0.14]];
-
-   //var markers = L.markerClusterGroup();
-
-
-   //for (let i=0; i<arr.length;i++) {
-   	//   console.log(i, arr[i][0],arr[i][1]);
-
-    //  let mark = L.marker([arr[i][0],arr[i][1]]).addTo(map);
-    //  markers.addLayer(mark);
-   //}
-
- //map.addLayer(markers);
-
-// Execute this function when advanced search button is pressed
- $scope.submit = function() {
-     console.log($scope);
- };
 
 
 
